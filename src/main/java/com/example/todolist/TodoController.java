fb6ae2a107c1e9b1d3c9d5af2b8783e5b253e94d
hamada <hamada1207.daiki@gmail.com>
2020-11-01 21:13:20 +0900

    create UPDATE

--- src/main/java/com/example/todolist/TodoController.java
+++ src/main/java/com/example/todolist/TodoController.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,6 +30,18 @@ public class TodoController {
   @PostMapping("/register")
   public String register(@ModelAttribute TodoForm formData) {
     todoService.setTodo(formData);
+    return "redirect:/";
+  }
+
+  @PostMapping("/update/{id}")
+  public String update(@PathVariable("id") Long todoId, @RequestParam String title) {
+    todoService.updateTodo(todoId, title);
+    return "redirect:/";
+  }
+
+  @PatchMapping("/done/{id}")
+  public String switchStatus(@PathVariable("id") Long todoId) {
+    todoService.switchTodo(todoId);
     return "redirect:/";
   }
 }
--- src/main/java/com/example/todolist/TodoService.java
+++ src/main/java/com/example/todolist/TodoService.java
@@ -6,6 +6,8 @@ import com.example.todolist.dao.TodoEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -23,5 +25,22 @@ public class TodoService {
     todo.setDeadline(formDate.getDeadline());
     todo.setStatus(false);
     todoRepository.save(todo);
+  }
+
+  public TodoEntity findTodoById(long todoId) {
+    Optional<TodoEntity> todoResult = todoRepository.findById(todoId);
+    return todoResult.get();
+  }
+
+  public void switchTodo(long todoId) {
+    TodoEntity todoEntity = findTodoById(todoId);
+    todoEntity.setStatus(!todoEntity.getStatus());
+    todoRepository.save(todoEntity);
+  }
+
+  public void updateTodo(long todoId, String title) {
+    TodoEntity todoEntity = findTodoById(todoId);
+    todoEntity.setTitle(title);
+    todoRepository.save(todoEntity);
   }
 }
